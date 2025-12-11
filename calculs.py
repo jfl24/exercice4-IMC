60a0008f51e1313f7c0c32d10007f238cbb7173f
Mevilen Ramsamy <mevilenramsamy@gmail.com>
2025-12-11 15:05:00 -0500

    Ajout historique IMC

--- calculs.py
+++ calculs.py
@@ -21,3 +21,27 @@ def interpreter_imc(imc):
 
 
 
+
+
+
+import os
+
+FICHIER_HISTORIQUE = "historique_imc.txt"
+
+def sauvegarder_calcul(nom, imc):
+    with open(FICHIER_HISTORIQUE, "a") as f:
+        f.write(f"{nom} : {imc:.2f}\n")
+
+def afficher_historique():
+    if not os.path.exists(FICHIER_HISTORIQUE):
+        print("Aucun calcul enregistré.")
+        return
+
+    print("=== Historique des calculs IMC ===")
+    with open(FICHIER_HISTORIQUE, "r") as f:
+        for ligne in f:
+            print(ligne.strip())
+
+
+
+
